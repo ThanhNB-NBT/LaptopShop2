42308af2c224a5a6526cef48ee13404516ac5a3f
thanhpe <nguyenthanh120223@gmail.com>
2024-05-16 14:36:31 +0700

    feat: UploadImageAPi

--- LaptopShop2/Areas/Admin/Controllers/TbProductsController.cs
+++ LaptopShop2/Areas/Admin/Controllers/TbProductsController.cs
@@ -8,6 +8,7 @@ using Microsoft.EntityFrameworkCore;
 using LaptopShop2.Models;
 using X.PagedList;
 using System.Data;
+using System.ComponentModel.DataAnnotations;
 
 namespace LaptopShop2.Areas.Admin.Controllers
 {
@@ -134,7 +135,7 @@ namespace LaptopShop2.Areas.Admin.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,Description,Detail,Image,CreatedDate,Price,Discount,Quantity,IsNew,IsBestSell,IsActive,Code,Origin,Size,Weight,Color,Material,CpuCompany,CpuType,CpuSpeed,CpuMaxSpeed,CpuCore,CpuProcessor,RamSize,RamType,RamSpeed,RamSupportMax,ScreenSize,ScreenPixel,ScreenPanel,CardBrand,CardModel,DriveType,DriveMemory,ConnectPort,Wifi,Bluetooth,Webcam,PinType,PinCapacity,Os,Version,CategoryProductId,BrandId")] TbProduct tbProduct, string CategoryInput)
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,Description,Detail,Image,CreatedDate,Price,Discount,Quantity,IsNew,IsBestSell,IsActive,Code,Origin,Size,Weight,Color,Material,CpuCompany,CpuType,CpuSpeed,CpuMaxSpeed,CpuCore,CpuProcessor,RamSize,RamType,RamSpeed,RamSupportMax,ScreenSize,ScreenPixel,ScreenPanel,CardBrand,CardModel,DriveType,DriveMemory,ConnectPort,Wifi,Bluetooth,Webcam,PinType,PinCapacity,Os,Version,CategoryProductId,BrandId")] TbProduct tbProduct)
         {
             if (id != tbProduct.ProductId)
             {
@@ -145,22 +146,7 @@ namespace LaptopShop2.Areas.Admin.Controllers
             {
                 try
                 {
-                    if (!string.IsNullOrEmpty(CategoryInput))
-                    {
-                        var CategoryName = _context.TbCategoryProducts.FirstOrDefault(c => c.Name == CategoryInput);
-                        if (CategoryName == null)
-                        {
-                            var newCategoryName = new TbCategoryProduct { Name = CategoryInput };
-                            _context.TbCategoryProducts.Add(newCategoryName);
-                            await _context.SaveChangesAsync();
-
-                            tbProduct.CategoryProductId = newCategoryName.CategoryProductId;
-                        }
-                        else
-                        {
-                            tbProduct.CategoryProductId = CategoryName.CategoryProductId;
-                        }
-                    }
+                    tbProduct.CreatedDate = DateTime.Now;
                     _context.Update(tbProduct);
                     await _context.SaveChangesAsync();
                     TempData["SuccessMessage"] = "Sửa sản phẩm thành công!";
@@ -231,6 +217,145 @@ namespace LaptopShop2.Areas.Admin.Controllers
         private bool TbProductExists(int id)
         {
             return _context.TbProducts.Any(e => e.ProductId == id);
+        }
+
+        //Upload hình ảnh
+        public class UploadOneFile
+        {
+            [Required(ErrorMessage = "Phải chọn file upload")]
+            [DataType(DataType.Upload)]
+            [FileExtensions(Extensions = "png,jpg,jpeg,gif,jfif,webp,svg")]
+            //[Display(Name = " Chọn file Upload")]
+            public IFormFile? FileUpload { get; set; }
+        }
+
+        [HttpGet]
+        public IActionResult UploadImage(int id)
+        {
+            var product = _context.TbProducts.Where(p => p.ProductId == id)
+                                            .Include(i => i.TbImages)
+                                            .FirstOrDefault();
+
+            if (product == null)
+            {
+                return NotFound("Không có sản phẩm");
+            }
+            ViewData["product"] = product;
+            return View(new UploadOneFile());
+        }
+
+        [HttpPost, ActionName("UploadImage")]
+        public async Task<IActionResult> UploadImageAsync(int id, [Bind("FileUpload")] UploadOneFile f)
+        {
+            var product = _context.TbProducts.Where(p => p.ProductId == id)
+                                            .Include(i => i.TbImages)
+                                            .FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound("Không có sản phẩm");
+            }
+            ViewData["product"] = product;
+
+            if (f != null)
+            {
+                var file1 = Path.GetFileNameWithoutExtension(Path.GetRandomFileName())
+                            + Path.GetExtension(f.FileUpload.FileName);
+                var file = Path.Combine("wwwroot", "files", file1);
+
+                using (var fileStream = new FileStream(file, FileMode.Create))
+                {
+                    await f.FileUpload.CopyToAsync(fileStream);
+                }
+                _context.Add(new TbImage()
+                {
+                    ProductId = product.ProductId,
+                    ImagePath = file1
+                });
+
+                await _context.SaveChangesAsync();
+
+            }
+
+            return View(f);
+        }
+
+        [HttpPost]
+        public IActionResult ListImages(int id)
+        {
+            var product = _context.TbProducts.Where(p => p.ProductId == id)
+                                            .Include(i => i.TbImages)
+                                            .FirstOrDefault();
+            if (product == null)
+            {
+                return Json(
+                    new
+                    {
+                        success = 0,
+                        message = "Product not found",
+                    }
+                );
+            }
+            var listImages = product.TbImages.Select(Image => new
+            {
+                id = Image.ImageId,
+                path = "/files/" + Image.ImagePath
+            });
+            return Json(
+                new
+                {
+                    success = 1,
+                    images = listImages,
+                }
+            );
+        }
+
+        [HttpPost]
+        public IActionResult DeleteImage(int id)
+        {
+            var image = _context.TbImages.Where(i => i.ImageId == id).FirstOrDefault();
+            if (image != null)
+            {
+                _context.Remove(image);
+                _context.SaveChanges();
+
+                var filename = "wwwroot/files/" + image.ImagePath;
+                System.IO.File.Delete(filename);
+            }
+            return Ok();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> UploadImageApi(int id, [Bind("FileUpload")] UploadOneFile f)
+        {
+            var product = _context.TbProducts.Where(p => p.ProductId == id)
+                                            .Include(i => i.TbImages)
+                                            .FirstOrDefault();
+            if (product == null)
+            {
+                return NotFound("Không có sản phẩm");
+            }
+
+            if (f != null)
+            {
+                var file1 = Path.GetFileNameWithoutExtension(Path.GetRandomFileName())
+                            + Path.GetExtension(f.FileUpload.FileName);
+                var file = Path.Combine("wwwroot", "files", file1);
+
+                using (var fileStream = new FileStream(file, FileMode.Create))
+                {
+                    await f.FileUpload.CopyToAsync(fileStream);
+                }
+                _context.Add(new TbImage()
+                {
+                    ProductId = product.ProductId,
+                    ImagePath = file1
+                });
+
+                await _context.SaveChangesAsync();
+
+            }
+            return Ok();
         }
     }
 }
--- LaptopShop2/Program.cs
+++ LaptopShop2/Program.cs
@@ -1,5 +1,6 @@
 using LaptopShop2.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +21,14 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseStaticFiles(new StaticFileOptions()
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files")
+    ),
+    RequestPath = "/files"
+});
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
